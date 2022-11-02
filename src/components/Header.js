d2275f06353558d1abab2894e3e7723efa4f440f
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-11-02 14:13:04 +0100

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
-
+import logo from "../assets/a-logo.png";
+import iconDown from "../assets/icon-down.png";
+import emptyCart from "../assets/empty-cart.png";
 class Header extends Component {
   render() {
     return (
@@ -9,6 +11,19 @@ class Header extends Component {
             <li key={category.name}>{category.name}</li>
           ))}
         </ul>
+
+        <img className="logo" src={logo} alt="store logo" />
+
+        <div className="currency-cart-container">
+					<button className="currency-changer">
+						$
+						<img src={iconDown} alt="icon-down" />
+					</button>
+
+					<button>
+						<img src={emptyCart} />
+					</button>
+        </div>
       </header>
     );
   }
